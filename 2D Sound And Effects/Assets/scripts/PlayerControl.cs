b015b39396406e625e92e1f620786632f8a8f3b7
deathgripsvegeta <sophiahowell831@gmail.com>
2022-10-14 11:30:09 -0700

    added jerma's dumpy

--- 2D Sound And Effects/Assets/scripts/PlayerControl.cs
+++ 2D Sound And Effects/Assets/scripts/PlayerControl.cs
@@ -8,11 +8,13 @@ public class PlayerControl : MonoBehaviour
     public bool isOnGround = false;
 
     private Rigidbody2D _playerRB;
+    private Animator _playerAnime;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerRB = GetComponent<Rigidbody2D>();
+        _playerAnime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class PlayerControl : MonoBehaviour
         {
             _playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
+            _playerAnime.SetBool("IsOnGround", false);
         }
     }
     
@@ -30,6 +33,7 @@ public class PlayerControl : MonoBehaviour
         if(other.gameObject.CompareTag("ground"))
         {
             isOnGround = true;
+            _playerAnime.SetBool("IsOnGround", true);
         }
     }
 }
